7347ba033f6eb1d6b682b26718b10c35b37b1110
Ken Sanislo <ken@intherack.com>
2017-01-12 20:11:13 -0800

    updated

--- tiny-webhook.py
+++ tiny-webhook.py
@@ -6,9 +6,9 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 import json
 import pprint
 import os
+import subprocess
 import sys
 
-HOOK_SECRET_KEY = os.environb[b'HOOK_SECRET_KEY']
 
 class GithubHookHandler(BaseHTTPRequestHandler):
     """Base class for webhook handlers.
@@ -30,29 +30,43 @@ class GithubHookHandler(BaseHTTPRequestHandler):
 
         if not self._validate_signature(post_data):
             self.send_response(401)
+            self.end_headers()
             return
 
         payload = json.loads(post_data.decode('utf-8'))
         self.handle_payload(self.headers['X-GitHub-Event'], payload)
-        self.send_response(202)
-        #self.send_header('Content-type','text/html')
-        self.end_headers()
-
 
 class MyHandler(GithubHookHandler):
     def handle_payload(self, event_type, json_payload):
-        """Simple handler for the json payload.
-           Events we should support are 'ping', 'push', and 'release'"""
-        print('Event: ' + event_type)
-
-        print('JSON payload')
-        pprint.pprint(json_payload)
-
-
+        """
+        Simple handler for the json payload.
+        """
+        repo_name = json_payload['repository']['full_name']
+        run_me = os.path.join(HOOK_SCRIPT_PATH, repo_name, event_type)
+        try:
+            subprocess.Popen(run_me)
+        except FileNotFoundError:
+            self.send_response(501)
+            status_message = 'Missing handler for ' + repo_name + ':' + event_type
+        except PermissionError:
+            self.send_response(500)
+            status_message = 'Permission denied on handler for ' + repo_name + ':' + event_type
+        else:
+            self.send_response(202)
+            status_message = 'Running handler for ' + repo_name + ':' + event_type
+        self.end_headers()
+        self.wfile.write(status_message.encode())
+ 
 if __name__ == '__main__':
     argparser = argparse.ArgumentParser(description='Github hook handler')
-    argparser.add_argument('port', type=int, help='TCP port to listen on')
+    argparser.add_argument('--secret', type=str, required=True, help='GitHub secret string')
+    argparser.add_argument('--port', type=int, default=6699, help='TCP port to listen on')
+    argparser.add_argument('--scripts', type=str, required=True, help='Path to handler scripts')
     args = argparser.parse_args()
+
+#HOOK_SECRET_KEY = os.environb[b'HOOK_SECRET_KEY']
+    HOOK_SECRET_KEY = args.secret.encode()
+    HOOK_SCRIPT_PATH = args.scripts
 
     server = HTTPServer(('', args.port), MyHandler)
     server.serve_forever()
